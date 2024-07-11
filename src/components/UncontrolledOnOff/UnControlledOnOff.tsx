17b06c688242fe5fc2c4133742d694e62303bade
IvanBliudnik <Bliudnik007@gmail.com>
2024-07-11 15:18:10 +0300

    myReact

--- src/components/UncontrolledOnOff/UnControlledOnOff.tsx
+++ src/components/UncontrolledOnOff/UnControlledOnOff.tsx
@@ -4,10 +4,10 @@ type OnOffPropsType = {
     onChange: (on: boolean) => void
 }
 
-export function UnControlledOnOff  (props: OnOffPropsType)  {
-    console.log('onOff rendering' )
+export function UnControlledOnOff(props: OnOffPropsType) {
+    console.log('onOff rendering')
     let [on, setOn] = useState<boolean>(false);
-    console.log("on:  " + on )
+    console.log("on:  " + on)
 
     const onStyle = {
         width: '30px',
@@ -15,7 +15,7 @@ export function UnControlledOnOff  (props: OnOffPropsType)  {
         border: '1px solid black',
         display: 'inline-block',
         padding: '2px',
-        backgroundColor:  on ? 'green' : "white",
+        backgroundColor: on ? 'green' : "white",
     };
     const offStyle = {
         width: '30px',
@@ -24,7 +24,7 @@ export function UnControlledOnOff  (props: OnOffPropsType)  {
         display: 'inline-block',
         marginLeft: '2px',
         padding: '2px',
-        backgroundColor:  on ? 'white' : "red",
+        backgroundColor: on ? 'white' : "red",
     };
     const indicatorStyle = {
         width: '10px',
@@ -33,13 +33,24 @@ export function UnControlledOnOff  (props: OnOffPropsType)  {
         border: '1px solid black',
         display: 'inline-block',
         marginLeft: '5px',
-        backgroundColor: on? 'green': 'red'
+        backgroundColor: on ? 'green' : 'red'
     };
+    const onClicked = () => {
+        setOn(true);
+        props.onChange(true)
+    }
+
+    const offClicked = () => {
+        setOn(false);
+        props.onChange(false)
+    }
     return (
         <div>
-            <div style={onStyle} onClick={()=>{setOn(true); props.onChange(true)}}>On</div>
-            <div style={offStyle} onClick={()=> {setOn(false); props.onChange(false)}}>Off</div>
-            <div style={indicatorStyle} ></div>
+            <div onClick={onClicked}>On
+            </div>
+            <div onClick={offClicked}>Off
+            </div>
+            <div style={indicatorStyle}></div>
         </div>
     );
 };
