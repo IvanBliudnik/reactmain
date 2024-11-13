1ca8570fc1bf22df86af5d24e0c44ceb39015f8f
IvanBliudnik <Bliudnik007@gmail.com>
2024-11-13 13:03:51 +0300

    21useEffect

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -7,15 +7,28 @@ export default {
 
 export const SimpleExample = () => {
     const [counter, setCounter] = React.useState(1);
+    const [fake, setFake] = React.useState(1);
     console.log("SimpleExample")
     useEffect(() => {
-        console.log("useEffect");
+        console.log("useEffect every time render");
         document.title = counter.toString();
     })
+    //без зависимости будет постоянно вызываться seEffect
+    useEffect(() => {
+        console.log("useEffect only first time render(componentDidMount)");
+        document.title = counter.toString();
+    }, [])
+    useEffect(() => {
+        console.log("useEffect first time render and every time {counter} change");
+        document.title = counter.toString();
+    }, [counter])
+    //, [counter] вызов только при загрузке страницы, потом только при изменении {counter}
     return (
         <>
-            Hello, {counter}
+            Hello, {counter} {fake}
             <button onClick={() => setCounter(counter + 1)}>+</button>
+            <button onClick={() => setFake(fake + 1)}>+</button>
         </>
     );
 };
+//useEffect отображается только после отображения страницы
