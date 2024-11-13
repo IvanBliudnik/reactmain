c0870721d828f8f553bcc9c1f208876158adbb71
IvanBliudnik <Bliudnik007@gmail.com>
2024-11-13 15:35:13 +0300

    21useEffect

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -3,8 +3,6 @@ import React, {useEffect} from 'react';
 export default {
     title: 'useEffect demo',
 };
-
-
 export const SimpleExample = () => {
     const [counter, setCounter] = React.useState(1);
     const [fake, setFake] = React.useState(1);
@@ -33,3 +31,40 @@ export const SimpleExample = () => {
     );
 };
 //сначала выполняется отрисовка компонента потом выполняется useEffect (но он регестрируется)
+export const SetTimeOutExample = () => {
+    const [counter, setCounter] = React.useState(1);
+    const [fake, setFake] = React.useState(1);
+    console.log("SetTimeOutExample")
+    useEffect(() => {
+        setTimeout(()=> {
+            console.log("SetTimeOutExample")
+            document.title = counter.toString();
+        }, 1000)
+    }, [counter])
+
+    return (
+        <>
+            Hello, {counter} {fake}
+            <button onClick={() => setCounter(counter + 1)}>+</button>
+            <button onClick={() => setFake(fake + 1)}>+</button>
+        </>
+    );
+};
+export const SetTimeExample = () => {
+    const [counter, setCounter] = React.useState(1);
+    const [fake, setFake] = React.useState(1);
+    console.log("SetTimeExample")
+    useEffect(() => {
+        setInterval(()=> {
+            setCounter(state => state + 1)
+        }, 1000)
+    }, [])
+
+    return (
+        <>
+            Hello,counter: {counter} - fake: {fake}
+            {/*<button onClick={() => setCounter(counter + 1)}>+</button>*/}
+            {/*<button onClick={() => setFake(fake + 1)}>+</button>*/}
+        </>
+    );
+};
