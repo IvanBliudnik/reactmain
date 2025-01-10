ce361973186858f3ee44ba7d59501f0ef0f45af5
IvanBliudnik <Bliudnik007@gmail.com>
2025-01-10 10:05:25 +0300

    Clock24

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -3,6 +3,7 @@ import React, {useEffect, useState} from 'react';
 export default {
     title: 'useEffect demo',
 };
+
 export const SimpleExample = () => {
     const [counter, setCounter] = React.useState(1);
     const [fake, setFake] = React.useState(1);
@@ -30,6 +31,7 @@ export const SimpleExample = () => {
         </>
     );
 };
+
 //сначала выполняется отрисовка компонента потом выполняется useEffect (но он регестрируется)
 export const SetTimeOutExample = () => {
     const [counter, setCounter] = React.useState(1);
@@ -50,23 +52,83 @@ export const SetTimeOutExample = () => {
         </>
     );
 };
-export const SetTimeExample = () => {
+
+export const SetIntervalExample = () => {
     const [counter, setCounter] = React.useState(1);
-    const [fake, setFake] = React.useState(1);
     console.log("SetTimeExample")
     useEffect(() => {
-        setInterval(()=> {
+        const intervalId = setInterval(()=> {
             setCounter(state => state + 1)
         }, 1000)
+        return () => {
+            clearInterval(intervalId);
+        }
     }, [])
-
     return (
         <>
-            Hello,counter: {counter} - fake: {fake}
-            {/*<button onClick={() => setCounter(counter + 1)}>+</button>*/}
-            {/*<button onClick={() => setFake(fake + 1)}>+</button>*/}
+            Hello,counter: {counter}
+            <button onClick={() => setCounter(counter + 1)}>+</button>
         </>
     );
 };
 
+export const ResetEffectExample = () => {
+    const [counter, setCounter] = React.useState(1);
+    console.log("Component rendered with: " + counter);
+    useEffect(() => {
+        console.log("Effect occurred: " + counter);
+        return () => { //функция зачистки
+            console.log("ResetEffect: " + counter);
+        }
+    }, [counter])
+    const increase = () => {
+        setCounter(counter + 1)
+    }
+    return (
+        <>
+            Hello, {counter}
+            <button onClick={increase}>+</button>
+        </>
+    );
+};
+
+export const KeysTrackerExample = () => {
+    const [text, setText] = useState("");
+    console.log("Component rendered with: " + text);
+    useEffect(() => {
+        const handler = (e:KeyboardEvent) => {
+            //задали обработчик события handler для использования в двух местах при событии и при зачистке
+            console.log(e.key)
+            setText(text + e.key)
+        }
+            window.addEventListener("keypress", handler)
+        return () => { //убиваем жизненный цикл компоненты если ушли от ёё перерисовки
+            window.removeEventListener("keypress", handler) //remove
+        }
+    }, [text])
+    return (
+        <>
+            Typed text: {text}
+        </>
+    );
+};
+
+export const SetTimeoutExample = () => {
+    const [text, setText] = useState("");
+    console.log("Component rendered with: " + text);
+    useEffect(() => {
+       const timeoutID = setTimeout(() => {
+            console.log("timeout expired")
+            setText("3 seconds passed ")
+        }, 3000)
+        return () => {
+            clearTimeout(timeoutID) //зачистка обязательна
+        }
+    }, [text])
+    return (
+        <>
+            Typed text: {text}
+        </>
+    );
+};
 
